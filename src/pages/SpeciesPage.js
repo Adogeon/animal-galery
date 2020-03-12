f1688d764ff4a739765e3f0c4054ccff0b582994
Anh Dao <addao823@gmail.com>
2020-03-12 15:09:48 -0700

    adding return button action and animation

--- src/pages/SpeciesPage.js
+++ src/pages/SpeciesPage.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import Header from "../components/Header";
 import content from "../data/species.json";
 
 const SpeciesPage = () => {
   const params = useParams();
   const data = content[params.key];
+  const history = useHistory();
+
+  const goBackOnClick = event => {
+    event.preventDefault();
+    history.goBack();
+  };
 
   return (
     <div className="wrapper">
       <Header title={data.name} />
       <div className="slide-wrapper">
         <div>
+          <div className="hid-box"></div>
           <div className="return-bar">
-            <a href="#">Close</a>
+            <button className="return-button"onClick={goBackOnClick}>
+              Close X
+            </button>
           </div>
           <main>
             <h1>{data.name}</h1>
